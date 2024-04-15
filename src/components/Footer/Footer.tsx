53cf56f31b47b31951ef24dc6992fd4ca603f7d6
eduardomds1996 <eduardomendoncasilva9@gmail.com>
2024-04-15 09:08:57 -0300

    fix: add margin footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,8 @@
 
 export default function Footer() {
     return (
-      <footer className="w-full bg-slate-100 flex justify-center items-center py-2">
+      <footer className="w-full bg-slate-100 flex justify-center items-center py-2 mt-48">
         <p className="text-sm">Desenvolvidor com muito 💚</p>
         </footer>
     );
   }
-  
